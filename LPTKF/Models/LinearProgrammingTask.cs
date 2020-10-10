8c6f554eac2281a5a063d44b48e6af987adc6b1d
Alexey <kozaa.19@uni-dubna.ru>
2020-10-10 11:13:47 +0300

    Добавил метод перевода MathExpressionSystem в LinearEquationSystem

--- LPTKF/Models/LinearProgrammingTask.cs
+++ LPTKF/Models/LinearProgrammingTask.cs
@@ -151,5 +151,6 @@ namespace LPTKF.Models {
         }
 
         
+
     }
 }
--- LPTKF/Models/MathExpressionSystem.cs
+++ LPTKF/Models/MathExpressionSystem.cs
@@ -49,5 +49,23 @@ namespace LPTKF.Models {
 
         public void Add(IMathExpression mathExpression) =>
             this.limits.Add(mathExpression);
+
+        /// <summary>
+        /// Перевод системы мат.выражений в систему линейных уравнений
+        /// </summary>
+        /// <returns>Систему линейныйх уравнений</returns>
+        public LinearEquationSystem ToLinearEquationSystem() {
+            int rows = this.limits.Count;
+            int columns = this.limits[0].VariableValues.Count + 1;
+            LinearEquationSystem result = new LinearEquationSystem(rows, columns);
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns - 1; j++)
+                    result[i, j] = this.limits[i][j];
+                result[i, columns - 1] = this.limits[i].FreeValue;
+            }
+
+            return result;
+        }
     }
 }
